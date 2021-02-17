9f4d77ffc3b1b1a4f6fa749b6e929048fcaf52ff
sullivan <sullytobias@gmail.com>
2021-02-17 16:18:39 +0100

    Add Tags And Links

--- crypto-app/src/components/details/basic.js
+++ crypto-app/src/components/details/basic.js
@@ -3,13 +3,20 @@ import React from 'react';
 import './basic.scss';
 
 const Basic = ({ details }) => {
+
+    const getUrlParsed = url => {
+        const hN = new URL(url).hostname;
+        return <a target='_blank' href={url}>{hN}</a>
+    }
+
     return (
         <div className='cmp-details-basic'>
             <div className='cmp-details-basic__marker'>
                 <img className='cmp-details-basic__marker--logo' src={details.image.small} alt={details.name} />
                 <div className='cmp-details-basic__marker__badges'>
-                    <span className='cmp-details-basic__marker__badges--rank'>Rank #{details.market_cap_rank}</span>
-                    <span className='cmp-details-basic__marker__badges--tags'>{details.categories.map(item => item)}</span>
+                    <div className='cmp-details-basic__marker__badges--rank'><span>Rank #{details.market_cap_rank}</span></div>
+                    <div className='cmp-details-basic__marker__badges--links'>{details.links.homepage.map(item => item && <span cmp-details-basic__marker__badges--links--link>{getUrlParsed(item)}</span>)}</div>
+                    <div className='cmp-details-basic__marker__badges--tags'>{details.categories.map(item => <span cmp-details-basic__marker__badges--tags--tag>{item}</span>)}</div>
                 </div>
             </div>
         </div>
